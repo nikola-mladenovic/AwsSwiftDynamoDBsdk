1cf074627e52eed425adc3024f3ebfd21f35b8f1
Nikola Mladenovic <nikola@mladenovic.biz>
2017-08-16 20:47:04 +0200

    Use master for AwsSwiftSign

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "AwsDynamoDB",
     products: [.library(name: "AwsDynamoDB", targets: ["AwsDynamoDB"])],
-    dependencies: [.package(url: "https://github.com/nikola-mladenovic/AwsSwiftSign.git", .branch("crypto-swift-fix"))],
+    dependencies: [.package(url: "https://github.com/nikola-mladenovic/AwsSwiftSign.git", .branch("master"))],
     targets: [.target(name: "AwsDynamoDB", dependencies: ["AwsSign"]),
               .testTarget(name: "AwsDynamoDBTests", dependencies: ["AwsDynamoDB"])]
 )
